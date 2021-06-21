72a4115905f001e7de1b92c4c1ab79e88889fb99
jarrisonfor <yegoma@outlook.com>
2021-06-21 01:25:50 +0100

    Seeders

---
+++ database/seeders/BaseSeeder.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+abstract class BaseSeeder extends Seeder
+{
+}
---
+++ database/seeders/TicketSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+class TicketSeeder extends BaseSeeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
